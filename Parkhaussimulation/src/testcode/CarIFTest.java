ac741c1faa512bf8f9203bfef215fe7c2b3ab9d7
jbaue <ssnooker98@gmail.com>
2020-08-18 20:02:43 +0200

    create Car IF Test

---
+++ Parkhaussimulation/src/testcode/CarIFTest.java
@@ -0,0 +1,31 @@
+package testcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import produktionscode.Car;
+import produktionscode.IF_Car;
+
+//Author: Marius Bauerfeind
+class CarIFTest {
+	
+	IF_Car a;
+	
+	@BeforeEach
+	void setup() {
+		a = new Car("1", "any");
+		
+	}
+	
+	
+	@Test
+	@DisplayName("Erstellung eines Autos und Get Methoden funktionieren")
+	void Autotest() {
+		assertEquals("any", a.getArt());
+		assertEquals("1", a.getID());
+	}
+
+}
