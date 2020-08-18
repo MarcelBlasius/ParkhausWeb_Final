6a3e4b48ca9459bb877a95a972c287d7df20c9cd
jbaue <ssnooker98@gmail.com>
2020-08-18 20:08:43 +0200

    create Car

---
+++ Parkhaussimulation/src/produktionscode/Car.java
@@ -0,0 +1,36 @@
+package produktionscode;
+
+//Author: Marius Bauerfeind
+public class Car implements IF_Car{
+	
+	private String id;
+	private String art;
+	private int parkplatz;
+	
+	public Car(String id, String art) {
+		this.id = id;
+		this.art = art;
+		
+	}
+
+
+	@Override
+	public String getArt() {
+		return art;
+	}
+
+
+	@Override
+	public String getID() {
+		return id;
+	}
+	
+	public void setParkplatz(int parkplatz) {
+		this.parkplatz = parkplatz;
+	}
+	
+	public int getParkplatz() {
+		return parkplatz;
+	}
+	
+}
