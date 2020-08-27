fa0dae5435cd9017284c90d800f60e383a636832
l-gebhard <lars.gebhard@smail.inf.h-brs.de>
2020-08-27 19:12:49 +0200

    Test für Stateklasse erstellt

--- Parkhaussimulation/src/produktionscode/IF_State.java
+++ Parkhaussimulation/src/produktionscode/IF_State.java
@@ -2,6 +2,8 @@ package produktionscode;
 
 public interface IF_State {
 	
+	
+	
 	public Double getGesamtEinnahmen();
 	public Double getAvgEinnahmen();
 	public Double getAvgParkdauer();
@@ -14,7 +16,7 @@ public interface IF_State {
 	public void setGesamtEinnahmen(Double gesamtEinnahmen);
 	public void setAvgEinnahmen(Double avgEinnahmen);
 	public void setAvgParkdauer(Double avgParkdauer);
-	public void setBesucheranzahl(Double besucheranzahl);
+	public void setBesucheranzahl(int besucheranzahl);
 	public void setMinEinnahmen(Double minEinnahmen);
 	public void setMinParkdauer(Double minParkdauer);
 	public void setMaxEinnahmen(Double maxEinnahmen);
--- Parkhaussimulation/src/testcode/StateTest.java
+++ Parkhaussimulation/src/testcode/StateTest.java
@@ -2,13 +2,63 @@ package testcode;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import produktionscode.IF_State;
+
 class StateTest {
+	
+	IF_State s = new State();
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Gesamteinnahme wird korrekt gespeichert")
+	void gesamtEinnahmenTest() {
+		s.setGesamtEinnahmen(10.5);
+		assertEquals(10.5,s.getGesamtEinnahmen());
+	}
+	
+	@Test
+	@DisplayName("Einnahmendurchschnitt wird korrekt gespeichert")
+	void avgEinnahmenTest() {
+		s.setAvgEinnahmen(10.5);
+		assertEquals(10.5,s.getAvgEinnahmen());
+	}
+	@Test
+	@DisplayName("Parkdauerdurchschnitt wird korrekt gespeichert")
+	void avgParkdauerTest() {
+		s.setAvgParkdauer(5.5);
+		assertEquals(5.5,s.getAvgParkdauer());
+	}
+	@Test
+	@DisplayName("Besucheranzahl wird korrekt gespeichert")
+	void besucheranzahlTest() {
+		s.setBesucheranzahl(7);;
+		assertEquals(7,s.getBesucheranzahl());
+	}
+	@Test
+	@DisplayName("Einnahmenminimum wird korrekt gespeichert")
+	void minEinnahmenTest() {
+		s.setMinEinnahmen(10.5);;
+		assertEquals(10.5,s.getMinEinnahmen());
+	}
+	@Test
+	@DisplayName("Parkdauerminimum wird korrekt gespeichert")
+	void minParkdauerTest() {
+		s.setMinParkdauer(5.5);;
+		assertEquals(5.5,s.getMinParkdauer());
+	}
+	@Test
+	@DisplayName("Einnahmenmaximum wird korrekt gespeichert")
+	void maxEinnahmenTest() {
+		s.setMaxEinnahmen(10.5);
+		assertEquals(10.5,s.getMaxEinnahmen());
+	}
+	@Test
+	@DisplayName("Parkdauermaximum wird korrekt gespeichert")
+	void maxParkdauerTest() {
+		s.setMaxParkdauer(5.5);
+		assertEquals(5.5,s.getMaxParkdauer());
 	}
 
 }
